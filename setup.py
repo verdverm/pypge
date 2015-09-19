da85e62bd0383163b43c86cd3b3ef3fa2bc000b2
Tony Worm <verdverm@gmail.com>
2015-09-19 17:04:23 -0400

    bumping version to 0.4

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from setuptools import setup, find_packages
 setup(
   name = 'pypge',
   packages = find_packages(), # this must be the same as the name above
-  version = '0.3',
+  version = '0.4',
   description = 'Prioritized Grammar Enumeration implementation',
   author = 'Tony Worm',
   author_email = 'verdverm@gmial.com',
   url = 'https://github.com/verdverm/pypge',
   keywords = ['pge', 'prioritized', 'grammar', 'enumeration', 'symbolic', 'regression', 'genetic programming'], # arbitrary keywords
   classifiers = [],
-  install_requires=['sympy', 'pandas', 'lmfit', 'deap'],
+  install_requires=['sympy', 'numpy', 'lmfit', 'sklearn', 'deap', 'networkx'],
 )
