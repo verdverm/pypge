ccf4b48ad2d6f1c5e135a202be1a72397ff9a4e9
Tony Worm <verdverm@gmail.com>
2016-03-20 11:45:06 -0600

    updates for new release

--- dockers/build.sh
+++ dockers/build.sh
@@ -6,19 +6,19 @@ set -e
 
 
 # BUILD base docker
-# docker build $nocache -t pypge/base      base
+# docker build $nocache -t verdverm/pypge-base      base
 
 # BUILD data docker
 # cp -r ../data data
-# docker build $nocache -t pypge/data      data
+# docker build $nocache -t verdverm/pypge-data      data
 
 # BUILD evaluator docker
 # rm -rf eval/evaluator && cp -r ../evaluator eval/evaluator
-# docker build $nocache -t pypge/eval      eval
+docker build $nocache -t verdverm/pypge-eval      eval
 
 # BUILD python, competitiors, and pypge dockers
-# docker build $nocache -t pypge/python2   python2
-# docker build $nocache -t pypge/python3   python3
+docker build $nocache -t verdverm/pypge-python2   python2
+docker build $nocache -t verdverm/pypge-python3   python3
 
-# docker build $nocache -t pypge/pypge-dev   pypge-dev
+docker build $nocache -t verdverm/pypge           pypge
 
